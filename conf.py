c66364a7da208591db5398152cfbbee6fc15e539
Patrik <rambo3swe@gmail.com>
2015-09-02 18:59:08 +0200

    lagt till conf

---
+++ conf.py
@@ -0,0 +1,27 @@
+import sys
+import os
+
+# The master toctree document.
+master_doc = 'index'
+
+# General information about the project.
+project = 'Code Standards'
+copyright = '2015, Ezy'
+author = 'Ezy'
+
+# The short X.Y version.
+version = '0.0.1'
+# The full version, including alpha/beta/rc tags.
+release = '0.0.1'
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+html_last_updated_fmt = '%b %d, %Y'
+
+# -- Options for Epub output ----------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
